9a91f9d91366e0698e92d8f41df46015d23320b6
Vincent Chang <vincenttchang90@gmail.com>
2017-05-11 17:30:21 -0400

    created player prototype

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(row, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = -100;
+    this.y = (row + 1) / 6 * canvas.height;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,11 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x > canvas.width) {
+        this.x = -100;
+    } else {
+        this.x = this.x + this.speed * dt;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -32,9 +40,43 @@ Enemy.prototype.render = function() {
 
 var Player = function() {
     this.sprite = 'images/char-boy.png';
+    this.x = 202;
+    this y = canvas.height - 171;
+}
+
+Player.prototype.handleInput = function(input) {
+    if (input === 'left'){
+        if (this.x > 0){
+            this.x -= 101;
+        }
+    } else if (input === 'up'){
+        this.y -= 101;
+        }
+    else if (input === 'right'){
+        if (this.x < 404){
+            this.x += 101;
+        }
+    } else if (input === 'down'){
+        if (this.y < 505){
+            this.y += 101;
+        }
+    }
+}
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 
+var allEnemies = [];
+var player = new Player();
+var enemy = new Enemy()
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
