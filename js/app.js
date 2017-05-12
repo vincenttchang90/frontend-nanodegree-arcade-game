557012344186da382385057434e710182e82db82
Vincent Chang <vincenttchang90@gmail.com>
2017-05-12 17:14:17 -0400

    got game working. need to fix user commands

--- js/app.js
+++ js/app.js
@@ -7,8 +7,8 @@ var Enemy = function(row, speed) {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = -100;
-    this.y = (row + 1) / 6 * canvas.height;
-    this.speed = speed;
+    this.y = row / 6 * 506 - 25;
+    this.speed = speed * getRandomInt(1,10);
 };
 
 // Update the enemy's position, required method for game
@@ -17,7 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    if (this.x > canvas.width) {
+    if (this.x > 505) {
         this.x = -100;
     } else {
         this.x = this.x + this.speed * dt;
@@ -41,28 +41,32 @@ Enemy.prototype.render = function() {
 var Player = function() {
     this.sprite = 'images/char-boy.png';
     this.x = 202;
-    this y = canvas.height - 171;
+    this.y = 404;
 }
 
 Player.prototype.handleInput = function(input) {
-    if (input === 'left'){
+    this.keyPress = input;
+}
+
+Player.prototype.update = function() {
+    if (this.keyPress === 'left'){
         if (this.x > 0){
             this.x -= 101;
         }
-    } else if (input === 'up'){
-        this.y -= 101;
+    } else if (this.keyPress === 'up'){
+        if (this.y > 0){
+            this.y -= 101;
         }
-    else if (input === 'right'){
+    } else if (this.keyPress === 'right'){
         if (this.x < 404){
             this.x += 101;
         }
-    } else if (input === 'down'){
-        if (this.y < 505){
+    } else if (this.keyPress === 'down'){
+        if (this.y < 404){
             this.y += 101;
         }
     }
 }
-
 Player.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -76,7 +80,15 @@ function getRandomInt(min, max) {
 
 var allEnemies = [];
 var player = new Player();
-var enemy = new Enemy()
+var getEnemies = function() {
+    for (var i = 1; i < 5; i++){
+        allEnemies.push(new Enemy(i, getRandomInt(10, 20)))
+    }
+}
+getEnemies();
+getEnemies();
+
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
